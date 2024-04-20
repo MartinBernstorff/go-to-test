4196824811756d4709aa3c69abd9f3e29620cc74
Martin Bernstorff <martinbernstorff@gmail.com>
2024-04-20 23:55:01 +0200

    update same-directory-strategy.ts

--- src/strategies/same-directory-strategy.ts
+++ src/strategies/same-directory-strategy.ts
@@ -1,10 +1,10 @@
 import Strategy from 'interfaces/strategy';
 
-const testPattern = /^(?<filePath>.*)\.test\.(?<ext>[^\.]+)$/;
+const testPattern = /^test_(?<filePath>.*)(?<ext>[^\.]+)$/;
 const sourcePattern = /^(?<filePath>.*)\.(?<ext>[^\.]+)$/;
 
 const sourceTemplate = '$<filePath>.$<ext>';
-const testTemplate = '$<filePath>.test.$<ext>';
+const testTemplate = 'test_$<filePath>.$<ext>';
 export default class SameDirectoryStrategy implements Strategy {
   public static getInstance() {
     return new SameDirectoryStrategy();
